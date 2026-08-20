12cbc51144f0f3450d7cfce576a636905214ac7e
Bill_Code_26 <biloliddinubaidulloev@gmail.com>
2026-08-20 12:20:12 +0500

    Fix formatting issues in FetchData.jsx

--- src/api/FetchData.jsx
+++ src/api/FetchData.jsx
@@ -1,7 +1,7 @@
 import plant1 from '../assets/plant.png'
 import plant2 from '../assets/plant.png'
 import plant3 from '../assets/plant.png'
-const api = "https://6a4b41e2f5eab0bb6b6275e5.mockapi.io/info";
+const api = "https://6a4b41e2f5eab0bb6b6275e5.mockapi.io/info"
 
 export async function FetchData(){
     const response = await fetch(api);
